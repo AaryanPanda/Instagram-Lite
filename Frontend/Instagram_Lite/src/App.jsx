a1698ed8cd518166846bc25b80ccd22b121c0674
aaryan <aaryanpanda04@gmail.com>
2024-07-30 09:44:26 +0530

    created login page userinterface

--- Frontend/Instagram_Lite/src/App.jsx
+++ Frontend/Instagram_Lite/src/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import SignUp from "./Pages/Signup.jsx"; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './Pages/Signup';
+import SignIn from './Pages/Signin';
+import Home from './Pages/Home';
 
 function App() {
-    return (
-        <div>
-            <SignUp />
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <div className="App"></div>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {' '}
+        </Route>
+        <Route path="/signup" element={<SignUp />}>
+          {' '}
+        </Route>
+        <Route path="/login" element={<SignIn />}>
+          {' '}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
