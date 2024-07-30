07068609ad42a71d91480dea7f7e1452c0d465ec
aaryan <aaryanpanda04@gmail.com>
2024-07-30 10:34:39 +0530

    Created a Responsive Navbar using Tailwind Classes

--- Frontend/Instagram_Lite/src/Pages/Home.jsx
+++ Frontend/Instagram_Lite/src/Pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import DesktopNav from '../Components/headers/DesktopNav/DesktopNav';
+import MobileNav from '../Components/headers/MobileNav/MobileNav';
 
 export default function Home() {
   return (
-    <div className="home">
-      <button>
-        <Link to={`/signup`}>Sign Up</Link></button>
+    <div className="home w-full min-h-screen">
+      <div className="w-full h-auto flex items-start justify-between lg:gap-x-32 md:gap-x-16 sm:gap-x-8 gap-x-4 relative">
+        {/* sidebar  */}
+        <div className="lg:w-[18%] sm:w-none md:w-none h-[100vh] pt-10 px-3 border-r border-r-gray-500 sticky top-0 left-0 lg:block sm:hidden md:block hidden">
+          <DesktopNav></DesktopNav>
+        </div>
+        <div className="w-full h-auto py-1 px-3 border-t border-t-[#1d1d1d] fixed bottom-0 left-0 lg:hidden md:hidden sm:block z-50">
+          <MobileNav></MobileNav>
+        </div>
+        {/* bottom navbar  */}
+        {/* feeds sections  */}
+      </div>
     </div>
   );
 }
