0f9a10b37b2f9cf939d95789e94a46e897ff55e2
aaryan <aaryanpanda04@gmail.com>
2024-08-03 23:18:59 +0530

    Created Profile Page

--- Frontend/Instagram_Lite/src/App.jsx
+++ Frontend/Instagram_Lite/src/App.jsx
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignUp from './Pages/Signup';
-import SignIn from './Pages/Signin';
-import Home from './Pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./Pages/Signup";
+import SignIn from "./Pages/Signin";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import AppLayout from "./Pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App"></div>
       <Routes>
-        <Route path="/" element={<Home />}>
-          {' '}
+        <Route path="/" element={<AppLayout/>}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
         </Route>
-        <Route path="/signup" element={<SignUp />}>
-          {' '}
-        </Route>
-        <Route path="/login" element={<SignIn />}>
-          {' '}
-        </Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
       </Routes>
     </BrowserRouter>
   );
