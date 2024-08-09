5469f847178c8af31a46b157e0b8e863efcae8d9
aaryan <aaryanpanda04@gmail.com>
2024-08-09 12:04:27 +0530

    Fetched all posts from backend

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -1,4 +1,4 @@
-const { Post } = require('../models/Index.js');
+const { Post, User } = require('../models/Index.js');
 const { body, validationResult } = require('express-validator');
 
 const validateCreatePost = [
@@ -14,9 +14,7 @@ const createPost = async (req, res) => {
   const { caption, image, hashtag } = req.body;
   const userId = req.user.id;
   try {
-    console.log("Creating post with:", { caption, image, hashtag, userId });
     const newPost = await Post.create({ caption, image, hashtag, userId });
-    console.log("Post created successfully:", newPost);
     res.status(201).json(newPost);
   } catch (error) {
     console.error('Error creating posts:', error);
@@ -24,4 +22,33 @@ const createPost = async (req, res) => {
   }
 };
 
-module.exports = { createPost, validateCreatePost };
+const getAllPost = async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          as: 'postedBy',
+          attributes: ['username'],
+        },
+      ],
+      order: [['createdAt', 'DESC']],
+    });
+    const formattedPosts = posts.map((post) => ({
+      id: post.id,
+      profileImg: 'https://cdn-icons-png.flaticon.com/128/3177/3177440.png',
+      username: post.postedBy.username,
+      time: post.createdAt,
+      postImg: post.image,
+      likeCount: 150,
+      commentCount: 20,
+      caption: post.caption,
+    }));
+    res.status(200).json(formattedPosts);
+  } catch (error) {
+    console.log('Error fetching posts :' + error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+module.exports = { createPost, validateCreatePost, getAllPost };
